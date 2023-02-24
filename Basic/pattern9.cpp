b212d11263bbd7398e53eead15596fa4818675c6
Janesh7 <janeshwalia273@gmail.com>
2023-02-24 23:03:02 +0530

    reverse triangle

---
+++ Basic/pattern9.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    int row = 1;
+
+    while(row <= n) {
+
+        //space print 
+        int space = n - row;
+        while(space) {
+            cout<<" ";
+            space = space - 1;
+        }
+
+        //stars print 
+        int col = 1;
+        while( col <= row ) {
+            cout<<"*";
+            col = col + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+    return 0;
+}
+//     *
+//    **
+//   ***
+//  ****
+// *****
