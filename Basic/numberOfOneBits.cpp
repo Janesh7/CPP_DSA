8fb0516e66bd64d255071857a09d6646e52e93a1
Janesh7 <janeshwalia273@gmail.com>
2023-02-27 10:42:53 +0530

    NumberOfOneBits

---
+++ Basic/numberOfOneBits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingWeight(int n) { // Use unsigned 32 bit integer for leetcode (uint32_t n) to remove compiler error
+    
+        int count = 0;
+        while(n!=0) {
+            
+            //checking last bit
+            if(n&1) { // bitwise AND with the last digit to see if its 1
+                count++;
+            }
+            n = n>>1; // shift right hence move the bit 
+        }
+        return count;
+    }
+};
