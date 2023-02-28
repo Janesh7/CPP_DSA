cfb6e69687f97c6707ff070e72a194d765652315
Janesh7 <janeshwalia273@gmail.com>
2023-02-28 11:26:26 +0530

    Readme

--- Leetcode Problems/subtractProdAndSum.cpp
+++ Leetcode Problems/subtractProdAndSum.cpp
@@ -1,4 +1,4 @@
-// LC problem no 1281
+// LC problem no 1281 - Given an integer number n, return the difference between the product of its digits and the sum of its digits.
 class Solution {
 public:
     int subtractProductAndSum(int n) {
