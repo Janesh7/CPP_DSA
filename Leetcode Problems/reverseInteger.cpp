99c96094c6c41d304f52afcbc1efc94d2be51fc6
Janesh7 <janeshwalia273@gmail.com>
2023-02-28 11:11:41 +0530

    revInt

---
+++ Leetcode Problems/reverseInteger.cpp
@@ -0,0 +1,23 @@
+// Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+#include<limits.h>
+
+class Solution {
+public:
+    int reverse(int x) {
+        
+        int ans  = 0;
+        while( x != 0) {
+            
+            int digit = x % 10;
+            
+            if( (ans > INT_MAX/10) || (ans < INT_MIN/10)) { // To return zero if number goes out of bounds, NOTE that ans*10(the operation which may lead to the overflow) would still give runtime error as it would overflow hence divide the bigger number rather
+                return 0;
+            }
+            
+            ans = (ans * 10) + digit;
+            x = x / 10;
+            
+        }
+        return ans;
+    }
+};
