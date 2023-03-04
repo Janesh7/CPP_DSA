9a0b4cf9248836a04e57d8c6463fc9d02571a94a
Janesh7 <janeshwalia273@gmail.com>
2023-03-04 14:25:04 +0530

    ArrayIntersect

---
+++ CodeStu/arrayIntersection.cpp
@@ -0,0 +1,37 @@
+#include<vector>
+
+// Problem Statement
+// You are given two arrays 'A' and 'B' of size 'N' and 'M' respectively. Both these arrays are sorted in non-decreasing order. You have to find the intersection of these two arrays.
+// Intersection of two arrays is an array that consists of all the common elements occurring in both arrays.
+// Note :
+// 1. The length of each array is greater than zero.
+// 2. Both the arrays are sorted in non-decreasing order.
+// 3. The output should be in the order of elements that occur in the original arrays.
+// 4. If there is no intersection present then return an empty array.
+
+// Basic approach:
+
+using namespace std;
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	int i = 0, j = 0;
+    vector<int> ans;
+    while(i<n && j<m) {
+    
+    if(arr1[i]==arr2[j])
+    {
+    	ans.push_back(arr1[i]);
+        i++;
+        j++;
+    }
+    else if (arr1[i] < arr2[j]) {
+  		i++;
+    }
+    else
+    {
+    	j++;
+    }
+    
+    }
+    return ans;
+}
