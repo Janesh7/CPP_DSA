2769fca13d2dc046dedbe06e4150a303e518a4b4
Janesh7 <janeshwalia273@gmail.com>
2023-03-08 19:53:01 +0530

    pivot

--- Binary Search/pivot.cpp
+++ Binary Search/pivot.cpp
@@ -1,21 +1,27 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include<iostream> 
 using namespace std;
 
-int find_pivot(vector<int> v) {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);    
-	int s = 0, e = v.size() - 1;
-	int mid=(s + e) / 2;
-	while (s < e)
-	{
-		if(v[mid] < v[mid+1])
-     			s=mid+1;
-    		else
-      			e = mid;
-    
-		mid = (s + e) / 2;
-	}
-	return s;
+int getPivot(int arr[], int n) {
+
+    int s = 0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+
+    while(s<e) {
+
+        if(arr[mid] >= arr[0])
+        {
+            s = mid+1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
+
+int main() {
+    int arr[5] = {1, 3, 8, 10, 17};
+    cout << "Pivot is " << getPivot(arr, 5) << endl;
 }
