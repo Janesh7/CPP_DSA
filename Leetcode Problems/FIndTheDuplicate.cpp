54f3a7fd088b64a32a9befead7f6337bfd14dc97
Janesh7 <janeshwalia273@gmail.com>
2023-03-09 14:25:48 +0530

    FIndTheDup

---
+++ Leetcode Problems/FIndTheDuplicate.cpp
@@ -0,0 +1,49 @@
+// NOT TAUGHT, REFER the soln
+
+// 287. Find the Duplicate Number
+// Medium
+// Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+// There is only one repeated number in nums, return this repeated number.
+
+// You must solve the problem without modifying the array nums and uses only constant extra space.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,3,4,2,2]
+// Output: 2
+// Example 2:
+
+// Input: nums = [3,1,3,4,2]
+// Output: 3
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 1, high = nums.size() - 1, cnt;
+        
+        while(low <=  high)
+        {
+            int mid = low + (high - low) / 2;
+            cnt = 0;
+            // cnt number less than equal to mid
+            for(int n : nums)
+            {
+                if(n <= mid)
+                    ++cnt;
+            }
+            // binary search on left
+            if(cnt <= mid)
+                low = mid + 1;
+            else
+            // binary search on right
+                high = mid - 1;
+            
+        }
+        return low;
+    }
+	// for github repository link go to my profile.
+};
