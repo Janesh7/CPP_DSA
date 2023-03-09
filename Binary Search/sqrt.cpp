c35c0f3b6010c446755341fcb2235e1fa39740bc
Janesh7 <janeshwalia273@gmail.com>
2023-03-09 20:27:34 +0530

    sqrt

--- Binary Search/sqrt.cpp
+++ Binary Search/sqrt.cpp
@@ -7,7 +7,7 @@ using namespace std;
         int e = n;
         long long int mid = s + (e-s)/2;
         
-        //  In the question we r given int as the value in this 
+        //  In the question we r given int as the value, here we r multiplying int with another int , to not overflow the int limit, we r using long long
 
         long long int ans = -1;
         while(s<=e) {
@@ -17,7 +17,7 @@ using namespace std;
             if(square == n)
                 return mid;
             
-            if(square < n ){
+            if(square < n ){ // if answer is lower than then the n therefore it has a chance being the sqrt therefore store it , if its greater than the number then no way it can be the sqtt(as the sqrt of n will always be in the range 1-n at max)
                 ans = mid;
                 s = mid+1;
             }
