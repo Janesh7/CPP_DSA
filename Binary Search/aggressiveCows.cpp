a3e0efd46c66076c9c211c30f348399630ce684a
Janesh7 <janeshwalia273@gmail.com>
2023-03-12 17:17:51 +0530

    aggrCow

--- Binary Search/aggressiveCows.cpp
+++ Binary Search/aggressiveCows.cpp
@@ -1,4 +1,34 @@
 
+// Aggressive Cows
+
+
+// Problem Statement
+// Suggest Edit
+// You are given an array consisting of 'N' integers which denote the position of a stall. You are also given an integer 'K' which denotes the number of aggressive cows. You are given the task of assigning stalls to 'K' cows such that the minimum distance between any two of them is the maximum possible.
+// Detailed explanation ( Input/output format, Notes, Constraints, Images )
+
+// Sample Input 1 :
+// 3 2
+// 1 2 3
+// Sample Output 1 :
+// 2
+// Explanation To Sample Input 1 :
+// The largest minimum distance will be 2 when 2 cows are placed at positions {1, 3}.
+// Sample Input 2 :
+// 6 4
+// 0 3 4 7 10 9
+// Sample Output 2 :
+// 3
+// Explanation To Sample Input 2 :
+// The largest minimum distance will be 3 when 4 cows are placed at positions {0, 3, 7, 10}.
+// Sample Input 3 :
+// 5 2
+// 4 2 1 3 6
+// Sample Output 3 :
+// 5
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
