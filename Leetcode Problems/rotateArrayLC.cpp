0e69ae5f01544574bf512c7d264ab1bf24b81adb
Janesh7 <janeshwalia273@gmail.com>
2023-03-14 20:06:22 +0530

    rotate

--- Leetcode Problems/rotateArrayLC.cpp
+++ Leetcode Problems/rotateArrayLC.cpp
@@ -36,6 +36,8 @@
 
 // Try to come up with as many solutions as you can. There are at least three different ways to solve this problem.
 // Could you do it in-place with O(1) extra space?
+#include<bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
