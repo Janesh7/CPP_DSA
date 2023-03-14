5daa6b3b03ffd27241423874400c77bad7f8ef70
Janesh7 <janeshwalia273@gmail.com>
2023-03-14 20:19:58 +0530

    rotateandsorted

---
+++ Array/checkSortedRotatedArray.cpp
@@ -0,0 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Appproach: There are 3 cases:
+// - It is an sorted array for ex {1,2,3,4,5}
+// If we check all the adj elements (circularly) then we find that ar[i-1]>ar[i] is true for only 1 instance ie for 5,1 for the last element
+// - It is rotated and sorted for ex {3,4,5,1,2}
+// If we check all the adj elements (circularly) then we find that ar[i-1]>ar[i] is true for only 1 instance ie for 5,1 
+// - It is not rotated and sorted ex {1,3,5,4,2}
+// Here more than 1 case exist which are true for ar[i-1]>ar[i] 
+
+// Hence we conclude that if there are more than one case for ar[i-1]>ar[i] then it is not rotated sorted
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int cnt = 0;
+        for(int i=1;i<nums.size();i++) 
+        {
+            if(nums[i] < nums[i-1])
+                cnt++;
+        }
+        if(nums[nums.size()-1] > nums[0])
+            cnt++;
+        
+        return cnt<=1;
+    }
+};
