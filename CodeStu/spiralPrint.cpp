224b437c2e4a21ff6fdd52efb55a6db3ce002b81
Janesh7 <janeshwalia273@gmail.com>
2023-03-26 17:04:56 +0530

    spiral print

--- CodeStu/spiralPrint.cpp
+++ CodeStu/spiralPrint.cpp
@@ -21,29 +21,29 @@ public:
         
         
         while(count < total) {
-            
-            //print starting row
+            // Also for every loop check the count condition as we dont want to get in a for loop and print elements even if the count has passed the total
+            //print starting row ------> right
             for(int index = startingCol; count < total && index<=endingCol; index++) {
                 ans.push_back(matrix[startingRow][index]);
                 count++;
             }
             startingRow++;
             
-            //print ending column
+            //print ending column  || down
             for(int index = startingRow; count < total && index<=endingRow; index++) {
                 ans.push_back(matrix[index][endingCol]);
                 count++;
             }
             endingCol--;
             
-            //print ending row
+            //print ending row   <------- left 
             for(int index = endingCol; count < total && index>=startingCol; index--) {
                 ans.push_back(matrix[endingRow][index]);
                 count++;
             }
             endingRow--;
             
-            //print starting column
+            //print starting column ^ ^ up
             for(int index = endingRow; count < total && index>=startingRow; index--) {
                 ans.push_back(matrix[index][startingCol]);
                 count++;
