de9b77f5ddc0913118f00325fc209b9551771d07
Janesh7 <janeshwalia273@gmail.com>
2023-03-26 17:06:44 +0530

    spiral print

---
+++ Leetcode Problems/spiralPrint.cpp
@@ -0,0 +1,78 @@
+
+// 54. Spiral Matrix
+// Given an m x n matrix, return all elements of the matrix in spiral order.
+
+// Example 1:
+
+
+// Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+// Output: [1,2,3,6,9,8,7,4,5]
+// Example 2:
+
+
+// Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+// Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 10
+// -100 <= matrix[i][j] <= 100
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int> >& matrix) {
+        
+        
+        vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int count = 0; // maintain count to keep chec whether all elements are printed or no
+        int total = row*col;
+        
+        //index initialisation
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row-1;
+        int endingCol = col-1;
+        
+        
+        while(count < total) {
+            // Also for every loop check the count condition as we dont want to get in a for loop and print elements even if the count has passed the total
+            //print starting row ------> right
+            for(int index = startingCol; count < total && index<=endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+            
+            //print ending column  || down
+            for(int index = startingRow; count < total && index<=endingRow; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+            
+            //print ending row   <------- left 
+            for(int index = endingCol; count < total && index>=startingCol; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+            
+            //print starting column ^ ^ up
+            for(int index = endingRow; count < total && index>=startingRow; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        } 
+        return ans;
+    }
+};
