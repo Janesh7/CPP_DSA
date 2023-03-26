37fec149c8ac917592d731b2f33292f5d4a8a654
Janesh7 <janeshwalia273@gmail.com>
2023-03-26 21:28:31 +0530

    Roate Matrix image

--- Leetcode Problems/RotateMatrixImage.cpp
+++ Leetcode Problems/RotateMatrixImage.cpp
@@ -6,7 +6,10 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& m) {
         int n=m.size();
-        
+        //  for 1 2 3
+		//      4 5 6
+		//  	7 8 9
+
         //Transpose of matrix
         for(int i=0;i<n;i++)
         {
@@ -15,12 +18,25 @@ public:
                 swap(m[i][j],m[j][i]);
             }
         }
+		// gives
+		// 		1 4 7 
+		// 		2 5 8 
+		// 		3 6 9
         
         // reverse the matrix
         for(int i=0;i<n;i++)
         {
             reverse(m[i].begin(),m[i].end());
         }
+		//  gives
+		// 7 4 1 
+		// 8 5 2 
+		// 9 6 3
+
+		//  if reverse operation is done on normal 1 2 3 matrix it gives
+		// 		3 2 1
+		//		6 5 4
+		// 		9 8 7
         
     }
 };
