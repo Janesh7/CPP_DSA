405d8ffacb4b811b9f702237349e97d638bfc580
Janesh7 <janeshwalia273@gmail.com>
2023-05-06 16:59:14 +0530

    Keypad problem leetcode

---
+++ Leetcode Problems/keypad.cpp
@@ -0,0 +1,69 @@
+// 17. Letter Combinations of a Phone Number
+// Medium
+
+
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
+ 
+
+// Constraints:
+
+// 0 <= digits.length <= 4
+// digits[i] is a digit in the range ['2', '9'].
+
+
+
+class Solution {
+private:
+    void solve(string digit, string output, int index, vector<string>& ans, string mapping[] ) {
+        
+        //base case
+        if(index >= digit.length()) {
+            ans.push_back(output);
+            return;
+        }
+
+        // We need index so subtract from '0' to get it 
+        int number = digit[index] - '0';
+        // Get the whole string which is mapped to the number for Ex. 2 -> 'abc'
+        string value = mapping[number];
+
+
+        // Traverse through the whole string, 1 by 1 add each and solve and at the last remove the element and continue the for loop as we want other char unique values as well, See diagram in the Recursion Lec 8 video 
+        for(int i=0; i<value.length(); i++) {
+            output.push_back(value[i]);
+            solve(digit, output, index+1, ans, mapping);
+            output.pop_back();
+        }
+        
+    }
+public:
+    vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        if(digits.length()==0)
+            return ans;
+        string output;
+        int index = 0;
+        // 0 and 1 doesnt have any mappings in the phone 
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+    }
+};
