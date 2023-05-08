bd76a794590b903e1754fc3cb1002c6f7fbaecb7
Janesh7 <janeshwalia273@gmail.com>
2023-05-08 22:19:23 +0530

    intro

--- 14. OOPs Concept/Lec 1/intro.cpp
+++ 14. OOPs Concept/Lec 1/intro.cpp
@@ -1,34 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Hero {
-    
-    //properties
-    private:
+class Hero
+{
+
+    // properties
+private:
     int health;
 
-    public:
+public:
     char *name;
     char level;
     static int timeToComplete;
 
-    Hero() {
+    Hero()
+    {
         cout << "Simple constructor called" << endl;
         name = new char[100];
     }
 
-    //Paramerterised Constructor
-    Hero(int health) {
-        this -> health = health;
+    // Paramerterised Constructor
+    Hero(int health)
+    {
+        this->health = health;
     }
 
-    Hero(int health, char level) {
-        this -> level = level;
-        this -> health = health;
+    Hero(int health, char level)
+    {
+        this->level = level;
+        this->health = health;
     }
 
-    //copy constructor
-    Hero(Hero& temp) {
+    // copy constructor
+    Hero(Hero &temp)
+    {
 
         char *ch = new char[strlen(temp.name) + 1];
         strcpy(ch, temp.name);
@@ -39,72 +44,69 @@ class Hero {
         this->level = temp.level;
     }
 
-    void print(){
+    void print()
+    {
         cout << endl;
-        cout << "[ Name: " << this->name<< " ,";
+        cout << "[ Name: " << this->name << " ,";
         cout << "health: " << this->health << " ,";
-        cout <<"level: " << this->level << " ]";
-        cout << endl << endl;
+        cout << "level: " << this->level << " ]";
+        cout << endl
+             << endl;
     }
 
-    int getHealth() {
+    int getHealth()
+    {
         return health;
     }
 
-    char getLevel() {
+    char getLevel()
+    {
         return level;
     }
 
-    void setHealth(int h) {
+    void setHealth(int h)
+    {
         health = h;
     }
 
-    void setLevel(char ch) {
+    void setLevel(char ch)
+    {
         level = ch;
     }
 
-    void setName(char name[]) {
+    void setName(char name[])
+    {
         strcpy(this->name, name);
     }
 
-    static int random() {
-        return timeToComplete ;
+    static int random()
+    {
+        return timeToComplete;
     }
 
-    //Destructor
-    ~Hero() {   
+    // Destructor
+    ~Hero()
+    {
         cout << "Destructor bhai called" << endl;
     }
-
 };
 
 int Hero::timeToComplete = 5;
 
-int main() {
+int main()
+{
 
-    //cout << Hero::timeToComplete << endl;
+    // cout << Hero::timeToComplete << endl;
     cout << Hero::random() << endl;
 
     // Hero a;
 
     // cout << a.timeToComplete << endl;
 
-
     // Hero b;
     // b.timeToComplete = 10 ;
     // cout  << a.timeToComplete << endl;
     // cout << b.timeToComplete << endl;
-
-
-
-
-
-
-
-
-
-
-
 
     // //Static
     // Hero a;
@@ -114,52 +116,31 @@ int main() {
     // //manually destructor call
     // delete b;
 
+    //     Hero hero1;
 
+    //     hero1.setHealth(12);
+    //     hero1.setLevel('D');
+    //     char name[7] = "Babbar";
+    //     hero1.setName(name);
 
+    //     //hero1.print();
 
+    //     //use default copy constructor
 
+    //     Hero hero2(hero1);
+    //     //hero2.print();
+    // //    Hero hero2 = hero1;
 
+    //     hero1.name[0] = 'G';
+    //     hero1.print();
 
-//     Hero hero1;
+    //     hero2.print();
 
-//     hero1.setHealth(12);
-//     hero1.setLevel('D');
-//     char name[7] = "Babbar";
-//     hero1.setName(name);
+    //     hero1 = hero2;
 
-//     //hero1.print();
+    //     hero1.print();
 
-//     //use default copy constructor
-
-//     Hero hero2(hero1);
-//     //hero2.print();
-// //    Hero hero2 = hero1;
-
-//     hero1.name[0] = 'G';
-//     hero1.print();
-
-//     hero2.print();
-
-//     hero1 = hero2;
-
-//     hero1.print();
-
-//     hero2.print();
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
+    //     hero2.print();
 
     // //Hero tt;
 
@@ -174,19 +155,6 @@ int main() {
 
     // Hero temp(22, 'B');
     // temp.print();
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /*
     //static allocation
@@ -207,11 +175,9 @@ int main() {
     cout << "level is  " << b->level << endl;
     cout << " health is " << b->getHealth() << endl;
     */
-    
-
 
     // //creation of Object
-    // Hero ramesh;   
+    // Hero ramesh;
     // cout << "Size of Ramesh is " << sizeof(ramesh) << endl;
 
     // cout << "Ramesh health is " << ramesh.getHealth() << endl;
@@ -219,12 +185,10 @@ int main() {
     // ramesh.setHealth(70);
     // ramesh.level = 'A';
 
-
-    // cout << "health is: " << ramesh.getHealth() << endl; 
-    // cout << "Level is: " << ramesh.level << endl; 
+    // cout << "health is: " << ramesh.getHealth() << endl;
+    // cout << "Level is: " << ramesh.level << endl;
 
     // //cout << "size : " << sizeof(h1) << endl;
-
 
     return 0;
 }
