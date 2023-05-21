d1d6e9608ad89579b6e3b67cf6ece23c907bb188
Janesh7 <janeshwalia273@gmail.com>
2023-05-21 18:45:02 +0530

    Lenght of the last word

---
+++ Leetcode Problems/Extras/Lenght of the last word.cpp
@@ -0,0 +1,56 @@
+// 58. Length of Last Word
+// Easy
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+// There will be at least one word in s.
+
+
+class Solution {
+
+public:
+
+    int lengthOfLastWord(string s) { 
+
+        int len = 0, tail = s.length() - 1;
+
+        while (tail >= 0 && s[tail] == ' ') tail--;
+
+        while (tail >= 0 && s[tail] != ' ') {
+
+            len++;
+
+            tail--;
+
+        }
+
+        return len;
+
+    }
+
+};
