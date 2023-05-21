0968476b143b0470832c61d097ac342f21ed0e98
Janesh7 <janeshwalia273@gmail.com>
2023-05-21 18:43:08 +0530

    remove duplicate from array

--- Leetcode Problems/Extras/Remove dup from arr.cpp
+++ Leetcode Problems/Extras/Remove dup from arr.cpp
@@ -1,13 +1,7 @@
 // 26. Remove Duplicates from Sorted Array
 // Easy
 
-// 10920
 
-// 14762
-
-// Add to List
-
-// Share
 // Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
 // Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
