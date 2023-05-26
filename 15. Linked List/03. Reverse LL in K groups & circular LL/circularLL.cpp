a9dd6cce56aa3b85b824d6acd22e0d947517251d
Janesh7 <janeshwalia273@gmail.com>
2023-05-26 19:56:43 +0530

    Circular LL

---
+++ 15. Linked List/03. Reverse LL in K groups & circular LL/circularLL.cpp
@@ -0,0 +1,17 @@
+bool isCircularList(Node* head) {
+    //empty list
+    if(head == NULL) {
+        return true;
+    }
+
+    Node* temp = head -> next;
+    while(temp != NULL && temp != head ) {
+        temp = temp -> next;
+    }
+
+    if(temp == head ) {
+        return true;
+    }
+
+    return false;
+}
