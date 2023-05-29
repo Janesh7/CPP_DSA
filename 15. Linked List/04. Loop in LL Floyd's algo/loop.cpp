fec93be54832bc8b88861ecb0ec917dfb2b42e52
Janesh7 <janeshwalia273@gmail.com>
2023-05-29 22:50:05 +0530

    Detect loop

---
+++ 15. Linked List/04. Loop in LL Floyd's algo/loop.cpp
@@ -0,0 +1,24 @@
+bool detectLoop(Node* head) {
+
+    if(head == NULL)
+        return false;
+
+    map<Node*, bool> visited;
+
+    Node* temp = head;
+
+    while(temp !=NULL) {
+
+        //cycle is present
+        if(visited[temp] == true) {
+            cout << "Present on element " << temp->data << endl;
+            return true;
+        }
+
+        visited[temp] = true;
+        temp = temp -> next;
+
+    }
+    return false;
+
+}
