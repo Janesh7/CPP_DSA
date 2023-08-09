7397a3e80e2a5f1146b01b6ded0892cad61e74e2
Janesh7 <janeshwalia273@gmail.com>
2023-08-09 12:04:44 +0530

    implement queue using stack

---
+++ 16. Stack/01. Basics/Q using Stack.cpp
@@ -0,0 +1,38 @@
+
+#include <stack>
+
+class QueueUsingStack {
+private:
+    std::stack<int> s1, s2;
+
+public:
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    int dequeue() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int front = s2.top();
+        s2.pop();
+        return front;
+    }
+
+    int front() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        return s2.top();
+    }
+
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
