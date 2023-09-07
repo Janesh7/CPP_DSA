c99108c5dccec12c85dfce5c6ad9a519ed58baa4
Janesh7 <janeshwalia273@gmail.com>
2023-09-07 20:44:40 +0530

    right view of tree

---
+++ Leetcode Problems/right view of tree.cpp
@@ -0,0 +1,60 @@
+// 199. Binary Tree Right Side View
+// Medium
+
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+// Example 1:
+
+
+// Input: root = [1,2,3,null,5,null,4]
+// Output: [1,3,4]
+// Example 2:
+
+// Input: root = [1,null,3]
+// Output: [1,3]
+// Example 3:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int> &ans, int level) {
+    //base case
+    if(root == NULL)
+        return ;
+       
+    //we entered into a new level    
+    if(level == ans.size())
+        ans.push_back(root->val);
+        
+    solve(root->right, ans, level+1);    
+    solve(root->left, ans, level+1);
+    
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        solve(root,ans,0);
+        return ans;
+    }
+};
